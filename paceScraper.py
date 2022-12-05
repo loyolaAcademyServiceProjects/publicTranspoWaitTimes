cea42ba75c4d345cc4029a5966dd313bd05fd0af
Seaver-OlsonLoyola <117297543+Seaver-OlsonLoyola@users.noreply.github.com>
2022-12-05 08:05:12 -0600

    Update paceScraper.py

--- paceScraper.py
+++ paceScraper.py
@@ -1,50 +1,45 @@
-
 '''
-stop : 984_75
-route {
-        421: 83 - 421 - Wilmette Avenue
-        422: 84 - 
-        423: 85
-}
-direction{
-        East: 1
-        North: 2
-        South: 3
-        West: 4
-}
-domain: tmweb.pacebus.com
-path: /TMWebWatch
+MIT License
 
-NOTES:
-    Possible exploit under jquery protocol
-        ```
-        var matches = (new RegExp(this._generatePrefix() + "=([^;]+);")).exec(document.cookie);
-            if (matches && document.location.protocol !== matches[1]) {
-               window.sessionStorage.clear();
-               this._initCache();
-               for (var key in this._cookieCache) {
-                   window.sessionStorage.setItem(key, this._cookieCache[key])
-               }
-            }
-        ```  Website checks if cookies have been changed and sets the key accordingly.  
-             Could create a false cookie and inject it using the same ID
+Copyright (c) 2022 Seaver Olson
 
-    Possible exploit using stored key values.  Inject on path /TMWebWatch to test auth walls?
-    ```
-    stop - MainContent_stopList_chosen
-    route - MainContent_routeList_chosen
-    driver.add_cookie({"name": "route", "value": "83", "domain":"tmweb.pacebus.com","path":"/TMWebWatch"})
-    ```
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 '''
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import keyboard
-import time
+import keyboard,time,pyairtable,colorama
 
-driver = webdriver.Chrome('/Users/Admin/Desktop/Pace/paceprogram/chromedriver')#change to path of chromedriver
+#pretty color
+RED = colorama.Fore.RED
+GREEN = colorama.Fore.GREEN
+
+
+#initialize Online Database
+api_key = ("keylqwDZvM1uGClmD")
+table = pyairtable.Table(api_key, 'appj1B1TakHpVkHpX', 'Times')
+
+#declare Driver Path and retrieve Bus Website URL
+driver = webdriver.Chrome('/Users/Admin/Desktop/Projects/Pace/paceprogram/chromedriver')#change to path of chromedriver
 driver.get("http://tmweb.pacebus.com/TMWebWatch/LiveArrivalTimes")
 
+#Basic Functions of route class| handles all keyboard outputs
 class route_functions:
     def __init__(self,route,directions):
         self.route = route
@@ -55,6 +50,7 @@ class route_functions:
         element.click()
         keyboard.write(text)
         keyboard.press_and_release("enter")
+        time.sleep(1)
     def direction(self,direction):
         #used to write direction - chose to use another funciton due to abstraction level
         element = driver.find_element(By.ID,"MainContent_directionList_chosen")
@@ -62,38 +58,46 @@ class route_functions:
         if direction.lower() == "west" or direction.lower() == "south":
             keyboard.press_and_release("down arrow")
         keyboard.press_and_release("enter")
+
 #sub-class used to clean up program and keep functions seperate from __main__
 class route(route_functions):
     def __init__(self,route,directions):
         super().__init__(route,directions)
         self.result = ""
+        self.ID =list()
+        #Dictionary of IDs and their matching Airtable ID
+        #NOTE probably a better way to do this 
+        airID ={'421East': 'rec4eeQbGOA6bh1qH','421West':'recWkb4t0PNGEID7m','422East':'recDCAGqFshuTSZWk','423South':'recKPs05Bvmi9Eces','422West':'recsedVWahwgBLqGh','423North':'recvEZY9Efi7GD9wd'}
         for direct in directions:
+            self.ID.append(route+direct)
             self.search('MainContent_routeList_chosen',self.route)
             self.direction(direct)
             self.search('MainContent_stopList_chosen','Loyola Academy')
-            time.sleep(1)
+            time.sleep(1)#gives connection time to load
             try:
                 result = driver.find_element(By.ID,"resultBox")
                 self.result += result.text
-            except:
-                print("ERROR: COULD NOT FIND TIME")
+            except: print(f"{RED}ERROR: COULD NOT FIND TIME")
         self.clean_result()
+        for i in range(1):#only ever 2 directions
+            table.update(airID.get(self.ID[i]),{'Arrival Times':self.times[i]})
+        print(f'{GREEN}Route ' + route + " has been loaded")
+
     def clean_result(self):
         times = []
         result = list(self.result.split("\n"))
-        for e in result:
-            if "Scheduled" not in e and "Last" not in e and "Next" not in e:
-                times.append(e)
-        self.times = times
-            
+        #handles Error Cases
+        if 'No upcoming' in result[0]:
+            self.times = ['Not in Route','Not in Route']
+        else:
+            for e in result:
+                if "Scheduled" not in e and "Last" not in e and "Next" not in e:
+                    times.append(e)
+            self.times = times
+
+#Object Declaration
 route421 = route('421',['East','West'])
 route422 = route('422',['East','West'])
 route423 = route('423',['North','South'])
 
-print("421:")
-print(route421.times)
-print("422:")
-print(route422.times)
-print("423:")
-print(route423.times)
 
