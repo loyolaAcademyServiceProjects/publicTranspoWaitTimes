d04d3a12b70a1e813d23f629adec68dd0ac2f7db
loyolaAcademyCS <swhalen@loy.org>
2023-02-13 10:56:17 -0600

    changed preReq.sh

--- preReq.sh
+++ preReq.sh
@@ -1,11 +1,11 @@
 if [[ "$OSTYPE" =~ ^msys ]]; then
-	PYVERSION="py"
+	PYEXE="py"
 else
-	PYVERSION="python3"
+	PYEXE="python3"
 fi
 
-$PYVERSION -m pip install --upgrade pip
-$PYVERSION -m pip uninstall selenium keyboard pyairtable beautifulsoup4
-$PYVERSION -m pip install selenium keyboard pyairtable beautifulsoup4
+$PYEXE -m pip install --upgrade pip
+$PYEXE -m pip uninstall selenium keyboard pyairtable beautifulsoup4
+$PYEXE -m pip install selenium keyboard pyairtable beautifulsoup4
 
 echo "Libraries have been installed"
