7f5dc2edf05f77768ffea02478ec8c29abbb5072
loyolaAcademyCS <swhalen@loy.org>
2023-02-13 10:35:51 -0600

    made MSYS accomdation in preReq.sh

--- preReq.sh
+++ preReq.sh
@@ -1,3 +1,11 @@
-python3 -m pip uninstall selenium keyboard pyairtable
-python3 -m pip install selenium keyboard pyairtable
+if [[ "$OSTYPE" =~ ^msys ]]; then
+	PYVERSION="py"
+else
+	PYVERSION="python3"
+fi
+
+$PYVERSION -m pip install --upgrade pip
+$PYVERSION -m pip uninstall selenium keyboard pyairtable beautifulsoup4
+$PYVERSION -m pip install selenium keyboard pyairtable beautifulsoup4
+
 echo "Libraries have been installed"
